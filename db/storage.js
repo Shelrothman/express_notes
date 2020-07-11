bc4a39b597ab6d32ef29f0168edc23a3359b6467
Q118 <shelbyfish91@gmail.com>
2020-07-11 16:54:27 -0700

    functions in storage.js

--- db/storage.js
+++ db/storage.js
@@ -3,6 +3,54 @@ const fs = require("fs");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-class Store {
+class Storage {
+    constructor() {
+        this.lastId = 0;
+    }
+
+    read() {
+        return readFileAsync("db/db.json", "utf8");
+    }
+
+    write(note) {
+        return  writeFileAsync("db/db.json", JSON.stringify(note));
+    }
+
+    getNotes() {
+        return this.read()
+        .then(notes => {
+            let parsedNotes;
+            try {
+                //concat to combine the notes into the array
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        })
+    }
+
+    addNotes(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error("Note 'title' & 'text' must not be blank!");
+        }
+
+        const newNote = { title, text, id: ++this.lastId };
+
+        return this.getNotes()
+        .then(notes => [...notes, newNote])//spread operator
+        .then(updatedNotes => this.write(updatedNotes))
+        .then(() => newNote);
+    }
+
+    removeNotes(id) {
+        return this.getNotes()
+        .then(notes => notes.filter(note => note.id !== parseInt(id)))
+        .then(filteredNotes => this.write(filteredNotes));
+    }
 
 };
+
+module.exports = new Storage();
