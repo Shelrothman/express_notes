5617fb389538946111eccc2a88488f9b71866448
Q118 <shelbyfish91@gmail.com>
2020-07-10 18:05:24 -0700

    proper routes for home vs notes

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.listen(PORT, () => {
 //     res.json(path.join(__dirname, "public/index.html"));
 //   });
 
-app.get("/notes", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-  });
+//app.get("/notes", function(req, res) {
+  //  res.json(path.join(__dirname, "notes/index.html"));
+  //});
