dd94dab8f5bee1f0dc0ff69cae452913bae411b2
Q118 <shelbyfish91@gmail.com>
2020-07-11 12:12:16 -0700

    fixed paths in htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -17,10 +17,10 @@ module.exports = function (app) {
 	// be unreachable.
 
 	app.get("/notes", (req, res) => {
-		res.sendFile(path.join(__dirname, "../notes.html"));
+		res.sendFile(path.join(__dirname, "../public/notes.html"));
 	});
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "../index.html"));
+		res.sendFile(path.join(__dirname, "../public/index.html"));
 	});
 };
