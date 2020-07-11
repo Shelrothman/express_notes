25c2cb982cc56c92958dc0709cac3cb7d3a9c90c
Q118 <shelbyfish91@gmail.com>
2020-07-11 12:34:40 -0700

    file change

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.json());
 //const serveStatic = require("serve-static");
 //middle-ware function
 app.use(express.static('public'));
-app.use(express.static('files'));
+//app.use(express.static('files'));
+
 // The code below points the server to a series of "route" files. These routes
 // give our server a "map" of how to respond when users visit or request data
 // from various URLs.
